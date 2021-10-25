5ece97d8e755f3736211af2936b01439f4e60078
Christina <batemanchristin@gmail.com>
2021-10-25 23:51:16 +0300

    add close mobile popup button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 document.getElementById('show-more').addEventListener('click', function () {
     document.getElementById('more-content').classList.toggle('show')
 })
+
+document.getElementById('close-mobile-footer-btn').addEventListener('click', function () {
+    document.getElementById('more-content').classList.toggle('show')
+})
